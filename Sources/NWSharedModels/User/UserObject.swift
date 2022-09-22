dea23e1caef975ca2d5730b7fc042bee111a324f
saroar <saroar9@gmail.com>
2022-09-22 12:33:56 +0300

    Here we NW go

---
+++ Sources/NWSharedModels/User/UserObject.swift
@@ -0,0 +1,81 @@
+
+import BSON
+
+public enum UserRole: String, Codable, CaseIterable {
+    case basic, superAdmin, englishAdmin, russianAdmin, banglaAdmin
+}
+
+public enum UserLanguage: String, Codable, CaseIterable {
+    case russian, english, bangla
+}
+
+public struct UserRoleAndLanguage: Encodable {
+    public init() {}
+
+    public let roles = UserRole.allCases
+    public let languages = UserLanguage.allCases
+}
+
+public struct UserCreateObject: Codable {
+    public var fullName: String
+    public var language: UserLanguage
+    public var role: UserRole = .basic
+
+    public var email: String?
+    public var phoneNumber: String?
+    public var passwordHash: String
+}
+
+public struct UserGetObject: Codable {
+
+    public let id: ObjectId?
+    public let fullName: String
+    public let phoneNumber: String?
+    public let email: String?
+    public let role: UserRole
+    public let language: UserLanguage
+
+    public init(
+        id: ObjectId? = nil,
+        fullName: String,
+        phoneNumber: String? = nil,
+        email: String? = nil,
+        role: UserRole,
+        language: UserLanguage
+    ) {
+        self.id = id
+        self.fullName = fullName
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.role = role
+        self.language = language
+    }
+}
+
+extension UserGetObject: Equatable {}
+
+public struct UserGetPublicObject: Codable {
+
+    public let id: ObjectId?
+    public let fullName: String
+    public let role: UserRole
+    public let language: UserLanguage
+
+    public init(
+        id: ObjectId? = nil,
+        fullName: String,
+        role: UserRole,
+        language: UserLanguage
+    ) {
+        self.id = id
+        self.fullName = fullName
+        self.role = role
+        self.language = language
+    }
+}
+
+extension UserGetPublicObject: Equatable {}
+
+extension UserGetPublicObject {
+    public static let demo: Self = .init(fullName: "Ami", role: .banglaAdmin, language: .english)
+}
