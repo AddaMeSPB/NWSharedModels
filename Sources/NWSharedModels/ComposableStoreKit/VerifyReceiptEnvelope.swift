1651e6cace1f5ce5d8c8fc3e30fa117f206f87d1
saroar <saroar9@gmail.com>
2022-10-24 11:54:37 +0300

    update ComposableStorekit

--- Sources/NWSharedModels/ComposableStoreKit/VerifyReceiptEnvelope.swift
+++ Sources/NWSharedModels/ComposableStoreKit/VerifyReceiptEnvelope.swift
@@ -1,7 +1,13 @@
+import StoreKit
+
 public struct VerifyReceiptEnvelope: Codable, Equatable {
   public let verifiedProductIds: [String]
 
   public init(verifiedProductIds: [String]) {
     self.verifiedProductIds = verifiedProductIds
   }
+}
+
+extension VerifyReceiptEnvelope {
+    static public let empty: VerifyReceiptEnvelope = .init(verifiedProductIds: [""])
 }
